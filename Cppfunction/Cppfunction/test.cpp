98059483a29df8fdadf32a99c921e2dc576e0b1d
张凯 <2748771839@qq.com>
2020-03-29 23:44:32 +0800

    c++函数的缺省/重载/引用/for的使用

--- Cppfunction/Cppfunction/test.cpp
+++ Cppfunction/Cppfunction/test.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 
+/*
 void TestFunc(int a = 10, int b = 20, int c = 30)
 {
 	cout << "a = " << a << endl;
 	cout << "b = " << b << endl;
 	cout << "c = " << c << endl;
 }	// 全缺省参数
+*/
 
 void TestFunc(int a, int b = 10, int c = 20)
 {
@@ -24,16 +26,51 @@ double Add(double left, double right)
 {
 	return left + right;
 }
+
 long Add(long left, long right)
 {
 	return left + right;
 }	// 函数重载
 
+#if 0
 int main()
 {
-	Add(10, 20);
-	Add(10.0, 20.0);
-	Add(10L, 20L);
+	// 引用的使用
+	int a = 0;
+	int& ra = a;
+	int& b = a;
+	int& c = b;
+	cout << &a << endl;
+	cout << &ra << endl;  
+	cout << &b << endl;
+	cout << &c << endl;
+	int x = 1;
+	c = x;
+	cout << &x << endl;
+	cout << &c << endl;
 
+	const int m = 10;
+	const int& n = m;
+
+	int r = 20;
+	int& s = c;
+	const int& e = c;
+}
+#endif
+
+void TestFor()
+{
+	int array[] = { 1, 2, 3, 4, 5 };
+	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+		array[i] *= 2;
+	for (int* p = array; p < array + sizeof(array) / sizeof(array[0]); ++p)
+		cout << *p << endl;
+}
+
+int main()
+{
+	int i = 0;
+	double d = i; // 隐式类型的转换
+	const double& rd = i;
 	return 0;
 }
