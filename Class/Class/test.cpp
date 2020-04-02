30706f92fe6875507381eb527db0dce54481c643
张凯 <2748771839@qq.com>
2020-04-02 22:57:20 +0800

    C++类与对象

--- Class/Class/test.cpp
+++ Class/Class/test.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
+#include<assert.h>
 #include <string.h>
 
 using namespace std;
@@ -91,49 +92,145 @@ int main()
 }
 */
 
-class Date
+//class Date
+//{
+//public:
+//	Date(int year = 0, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}	// 构造函数
+//	
+//
+//	// Date
+//	// 两个构造函数构成函数重载
+//	
+//	/*
+//	Date()
+//	{
+//		_year = 1900;
+//		_month = 1;
+//		_day = 1;
+//	}
+//	*/
+//
+//	void Display()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//
+//	void SetDate(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year; // 年 _建议成员变量
+//	int _month; // 月
+//	int _day; // 日
+//};
+
+//int main()
+//{
+//	Date d1;
+//	d1.Display();
+//	Date d2(2020);
+//	d2.Display();
+//	Date d3(2020,4);
+//	d3.Display();
+//	Date d4(2020, 4, 2);
+//	d4.Display();
+//	return 0;
+//}	// 日期类
+
+/*
+class Time
 {
 public:
-	Date(int year, int month, int day)
+	Time()
 	{
-		_year = year;
-		_month = month;
-		_day = day;
-	}	// 构造函数
-
-	// Date
-	// 两个构造函数构成函数重载
-	Date()
-	{
-		_year = 1900;
-		_month = 1;
-		_day = 1;
-	}
-
-	void Display()
-	{
-		cout << _year << "-" << _month << "-" << _day << endl;
-	}
-
-	void SetDate(int year, int month, int day)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
+		cout << "Time()" << endl;
+		_hour = 0;
+		_minute = 0;
+		_second = 0;
 	}
 private:
-	int _year; // 年 _建议成员变量
-	int _month; // 月
-	int _day; // 日
+	int _hour;
+	int _minute;
+	int _second;
 };
-
+class Date
+{
+private:
+	// 基本类型(内置类型)
+	int _year;
+	int _month;
+	int _day;
+	// 自定义类型
+	Time _t;
+};
 int main()
 {
-	Date d1(2020, 4, 1);
-	d1.Display();
-	Date d2(2020, 4, 2);
-	d2.Display();
-	Date d3;
-	d3.Display();
+	Date d;
 	return 0;
-}	// 日期类
+}
+*/
+
+typedef int DataType;
+class SeqList
+{
+public:
+
+	SeqList(int capacity = 10)
+	{
+		_pData = (DataType*)malloc(capacity * sizeof(DataType));
+		assert(_pData);
+		_size = 0;
+		_capacity = capacity;
+	}
+
+	~SeqList()
+	{
+		if (_pData)
+		{
+			free(_pData); // 释放堆上的空间
+			_pData = NULL; // 将指针置为空
+			_capacity = 0;
+			_size = 0;
+		}
+	}
+private:
+	int* _pData;
+	size_t _size;
+	size_t _capacity;
+};
+
+class String
+{
+public:
+	String(const char* str = "jack")
+	{
+		_str = (char*)malloc(strlen(str) + 1);
+		strcpy(_str, str);
+	}
+	~String()
+	{
+		cout << "~String()" << endl;
+		free(_str);
+	}
+private:
+	char* _str;
+};
+class Person
+{
+private:
+	String _name;
+	int _age;
+};
+int main()
+{
+	Person p;
+	return 0;
+}
