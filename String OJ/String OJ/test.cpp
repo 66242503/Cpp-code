9c54442f8c6fca275a20a7e3c092021d085984c5
张凯 <2748771839@qq.com>
2020-04-12 23:43:41 +0800

    string类(字符串)的相关OJ习题

--- String OJ/String OJ/test.cpp
+++ String OJ/String OJ/test.cpp
@@ -34,7 +34,7 @@ https://leetcode-cn.com/problems/first-unique-character-in-a-string/
 */
 
 /*
-解法1：
+解法一：
 正反查询，如果下标相同就返回
 优点：代码简洁易懂
 缺点：时间复杂度为O(N^2)
@@ -52,7 +52,7 @@ public:
 };
 
 /*
-解法2：
+解法二：
 计数排序思想,遍历字符串之后算出每个字母出现的个数，字母个数是1的返回下标
 优点：时间复杂度为O(N),效率较高
 缺点：代码偏复杂
@@ -84,6 +84,56 @@ public:
 链接：https://leetcode-cn.com/problems/valid-palindrome/
 */
 
+class Solution {
+public:
+	bool ISCharOrNumber(char ch)
+	{
+		if ((ch >= 'a' && ch <= 'z')
+			|| (ch >= 'A' && ch <= 'Z')
+			|| (ch >= '0' && ch <= '9'))
+			return true;
+		else
+			return false;
+	}
+
+	bool isPalindrome(string s) {
+		int left = 0;
+		int right = s.size() - 1;
+		while (left < right)
+		{
+			while (left < right && ISCharOrNumber(s[left]) == false)
+				++left;
+
+			while (left < right && ISCharOrNumber(s[right]) == false)
+				--right;
+
+			if (s[left] != s[right])
+			{
+				if (s[left] < s[right] && s[left] >= 'A' && s[left] + 32 == s[right])
+				{
+					++left;
+					--right;
+				}
+				else if (s[left] > s[right] && s[right] >= 'A' && s[right] + 32 == s[left])
+				{
+					++left;
+					--right;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				++left;
+				--right;
+			}
+		}
+		return true;
+	}
+};
+
 
 
 /*
@@ -92,17 +142,24 @@ public:
 https://leetcode-cn.com/problems/reverse-string/
 */
 
+// 解法一：直接用reverse算法逆置字符串
 class Solution {
 public:
 	void reverseString(vector<char>& s)
 	{
 		reverse(s.begin(), s.end());
-		// int left = 0;
-		// int right = s.size() - 1;
-		// while(left < right)
-		// {
-		//     swap(s[left++],s[right--]);
-		// }
+	}
+};
+
+// 解法二：利用首尾交换原则，将字符串依次交换
+class Solution {
+public:
+	void reverseString(vector<char>& s)
+	{
+		int left = 0;
+		int right = s.size() - 1;
+		while (left < right)
+			swap(s[left++], s[right--]);
 	}
 };
 
@@ -146,6 +203,46 @@ public:
 链接：https://leetcode-cn.com/problems/add-strings/
 */
 
+class Solution {
+public:
+	string addStrings(string num1, string num2) {
+		string retStr;
+		retStr.reserve(max(num1.size(), num2.size()));
+		int next = 0;
+		int right1 = num1.size() - 1;
+		int right2 = num2.size() - 1;
+		while (right1 >= 0 || right2 >= 0)
+		{
+			int val1 = 0;
+			if (right1 >= 0)
+				val1 = num1[right1] - '0';
+
+			int val2 = 0;
+			if (right2 >= 0)
+				val2 = num2[right2] - '0';
+
+			int ret = val1 + val2 + next;
+			if (ret > 9)
+			{
+				ret -= 10;
+				next = 1;
+			}
+			else
+			{
+				next = 0;
+			}
+			retStr += ('0' + ret);
+			--right1;
+			--right2;
+		}
+		if (next == 1)
+			retStr += '1';
+
+		reverse(retStr.begin(), retStr.end());
+		return retStr;
+	}
+};
+
 
 
 /*
@@ -153,3 +250,27 @@ public:
 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
 链接：https://leetcode-cn.com/problems/multiply-strings/description/
 */
+
+class Solution {
+public:
+	string multiply(string num1, string num2) {
+		int end1 = num1.size();
+		int end2 = num2.size();
+		string ret(end1 + end2, '0');
+		for (int i = end2 - 1; i >= 0; i--)
+		{
+			for (int j = end1 - 1; j >= 0; j--)
+			{
+				int tmp = (ret[i + j + 1] - '0') + (num1[j] - '0') * (num2[i] - '0');
+				ret[i + j + 1] = tmp % 10 + '0';
+				ret[i + j] += tmp / 10;
+			}
+		}
+		for (int i = 0; i < end1 + end2; i++)
+		{
+			if (ret[i] != '0')
+				return ret.substr(i);
+		}
+		return "0";
+	}
+};
