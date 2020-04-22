70f9a51b7c57522a438c5f1e6cbea12c8d180947
张凯 <2748771839@qq.com>
2020-04-22 23:42:16 +0800

    string类的模拟实现

---
+++ String_Simulation/String_Simulation/String_Simulation.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+using namespace std;
+
+class string
+{
+public:
+	string();
+	~string();
+
+private:
+
+};
+
+string::string()
+{
+}
+
+string::~string()
+{
+}
