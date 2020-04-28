adafc8e7525cb793ef35162e1b51098f1c926d97
张凯 <2748771839@qq.com>
2020-04-28 23:29:29 +0800

    C/C++内存管理

--- Function_Template/Function_Template/Function_Template.cpp
+++ Function_Template/Function_Template/Function_Template.cpp
@@ -44,7 +44,7 @@ int main()
 	Swap(c, d);
 	return 0;
 }
-#endif
+
 
 template<class T>
 T Add(const T& left, const T& right)
@@ -77,4 +77,23 @@ int main()
 	// 显式实例化
 	Add<int>(a, b);
 	return 0;
+}
+
+#endif
+
+// 专门处理int的加法函数
+int Add(int left, int right)
+{
+	return left + right;
+}
+// 通用加法函数
+template<class T>
+T Add(T left, T right)
+{
+	return left + right;
+}
+void Test()
+{
+	Add(1, 2); // 与非模板函数匹配，编译器不需要特化
+	Add<int>(1, 2); // 调用编译器特化的Add版本
 }
