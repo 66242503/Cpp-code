c52ff27270b51458981047d94947f81d8d2afac7
张凯 <2748771839@qq.com>
2020-05-24 23:32:39 +0800

    string类的使用

--- String/String/string.cpp
+++ String/String/string.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 void Test1()
 {
 	string s1;
@@ -143,7 +142,7 @@ void Test5()
 	str1 += ' ';
 	str1 += str2;
 	cout << str1 << endl;
-}	// 字符串的修改，尾插
+}	// 字符串的修改，尾插(operator)
 
 
 void Test6()
@@ -168,9 +167,16 @@ void Test6()
 }
 
 
-void Test6()
+void Test7()
 {
+	string s("hello world");
+	s.insert(5,"x");
+	cout << s << endl;
+
+	s.erase();
+	cout << s << endl;
 	// 插入、删除操作
+
 }
 
 
@@ -192,98 +198,6 @@ void Func(const string& s)
 
 int main()
 {
-	Test6();
+	Test7();
 	return 0;
 }
-
-
-#if 0
-namespace N
-{
-	// 实现一个简单的string类
-	class string
-	{
-	public:
-		string(char* str = "")
-			:_str(new char[strlen(str)+1])
-		{
-			strcpy(_str, str);
-		}
-		// string copy(s1)
-		string(const string& s)
-			:_str(new char[strlen(s._str)+1])
-		{
-			strcpy(_str, s._str);
-		}
-
-		// copy = s1
-		// s1 = s1
-		string& operator=(const string& s)
-		{
-			if (this != &s)
-			{
-				delete[] _str;
-				_str = new char[strlen(s._str) + 1];
-				strcpy(_str, s._str);
-			}
-
-			return *this;
-		}
-
-		~string()
-		{
-			delete[] _str;
-			_str = nullptr;
-		}
-
-		char& operator[](size_t pos)
-		{
-			return _str[pos];
-		}
-
-		const char& operator[](size_t pos) const
-		{
-			return _str[pos];
-		}
-
-		size_t size() const
-		{
-			return strlen(_str);
-		}
-
-	private:
-		char* _str;
-	};
-
-	ostream& operator<<(ostream& out, const string& s)
-	{
-		for (size_t i = 0; i < s.size(); ++i)
-		{
-			out << s[i];
-		}
-		return out;
-	}
-}
-
-//int main()
-//{
-//	N::string s1("world");
-//	N::string s2("hello");
-//	s2[0] = 'x';
-//	cout << sizeof(s1) << endl;
-//	cout << sizeof(s2) << endl;
-//
-//	N::string copy(s2);
-//	cout << s1 << endl;
-//	cout << s2 << endl;
-//	cout << copy << endl;
-//
-//	copy = s1;
-//	s1 = s1;
-//	cout << s1 << endl;
-//	cout << s2 << endl;
-//	cout << copy << endl;
-//
-//	return 0;
-//}
-#endif
