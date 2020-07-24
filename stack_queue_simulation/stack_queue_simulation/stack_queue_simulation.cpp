04d86d77043fa6e020074220098d14e163e637d2
张凯 <2748771839@qq.com>
2020-07-24 16:25:36 +0800

    栈与队列的模拟实现

--- stack_queue_simulation/stack_queue_simulation/stack_queue_simulation.cpp
+++ stack_queue_simulation/stack_queue_simulation/stack_queue_simulation.cpp
@@ -1,1 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+
+#include <list>
+#include <iostream>
+#include <vector>
+
+namespace st
+{
+
+	template <class T, class Container>
+
+	class stack
+	{
+	public:
+		void push(const T& x){ _c.push_back(x); }
+		void pop(){ _c.pop_back(); }
+		size_t size() { return _c.size(); }
+		bool empty(){ return _c.empty(); }
+		T& top(){ return _c.back(); }
+	private:
+		Container _c;
+	};
+}
+
+
+namespace que
+{
+	template <class T, class Container>
+	class queue
+	{
+	public:
+		void push(const T& x){ _c.push_back(x); }
+		void pop(){ _c.pop_front(); }
+		size_t size() { return _c.size(); }
+		bool empty(){ return _c.empty(); }
+		T& front(){ return _c.front(); }
+		T& back(){ return _c.back(); }
+	private:
+		Container _c;
+	};
+}
