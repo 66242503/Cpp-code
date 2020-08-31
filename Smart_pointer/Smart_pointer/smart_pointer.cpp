5873f8d994a1a0514d4218525e9b071ff1857ccb
张凯 <2748771839@qq.com>
2020-08-31 23:47:10 +0800

    智能指针的模拟实现

--- Smart_pointer/Smart_pointer/smart_pointer.cpp
+++ Smart_pointer/Smart_pointer/smart_pointer.cpp
@@ -1,7 +1,74 @@
 #include <iostream>
+#include "SmartPtr.h"
+
+using namespace std;
+#if 0
+// 抛异常导致的内存泄露及解决方式
+int div()
+{
+	int a, b;
+	cin >> a >> b;
+	if (b == 0)
+		throw invalid_argument("除0错误");
+	return a / b;
+}
+
+void f1()
+{
+	int *p = new int;
+	cout << div() << endl;
+	delete p;
+	try
+	{
+		cout << div() << endl;
+	}
+	catch (...)
+	{
+		delete p;
+		throw;
+	}
+}
 
 int main()
 {
+	try
+	{
+		f1();
+	}
+	catch (exception& e)
+	{
+		cout << e.what() << endl;
+	}
+	return 0;
+}
+#endif
 
+// 智能指针
+int div()
+{
+	int a, b;
+	cin >> a >> b;
+	if (b == 0)
+		throw invalid_argument("除0错误");
+	return a / b;
+}
+
+void f1()
+{
+	int *p = new int;
+	SmartPtr<int> sp(p);
+	cout << div() << endl;
+}
+
+int main()
+{
+	try
+	{
+		f1();
+	}
+	catch (exception& e)
+	{
+		cout << e.what() << endl;
+	}
 	return 0;
 }
